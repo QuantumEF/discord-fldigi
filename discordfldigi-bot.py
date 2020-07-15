4b3801636c8bbc62eadcfd071b0c499a8689507e
QuantumEF <quantumerikfong@gmail.com>
2020-07-14 23:43:39 -0400

    Updated
    Added support for multiple channels. Have a ping system. 1 less runtime error.

--- discordfldigi-bot.py
+++ discordfldigi-bot.py
@@ -8,11 +8,13 @@ from bidict import bidict
 client = discord.Client()
 fldigi = xmlrpc.client.ServerProxy('http://{}:{}/'.format('127.0.0.1', 7362), transport=RequestsTransport(use_builtin_types=True), allow_none=True)
 
-callsign_dictionary = {} #Map "Discord User ID":"Callsign"  Note: The ID must be in string form ie "12334455667788"
+callsign_dictionary = {
+                        "discord_id" : "callsign"
+                      }
 
-discord_channel_id = 000000000000 #Discord channel id to read and write to. In numberic form
+discord_channel_ids = [00000000000000]
 
-bot_token = 'Your Super Secret Bot Token'
+bot_token = 'A Secret'
 
 callsign_bidict = bidict(callsign_dictionary)
 
@@ -26,31 +28,38 @@ async def on_ready():
     while True:
         await asyncio.sleep(1)
         text = fldigi.rx.get_data()
-        if text != b'':
-             try:
-                 await channel.send(text.decode("utf-8"))
-             except UnicodeDecodeError:
-                 print("Invalid Decode")
-                 continue
-        text2 = text.decode("utf-8").upper()
+        text = text.decode("utf-8")
+        if text != '':
+            print(text)
+            try:
+                await channel.send(text)
+            except UnicodeDecodeError:
+                print("Invalid Decode")
+                continue
+        text2 = text.upper()
         for key in callsign_bidict.inverse:
-            if key in text2:
+            if "@"+key in text2:
                 await channel.send("<@!"+callsign_bidict.inverse[key]+">")
-        if fldigi.main.get_trx_status() == 'rx':
-            await client.change_presence(status=discord.Status.online)
-        else:
+        if " PING " in " "+text2:
+            fldigi.text.add_tx('PONG de KN4VHM (Auto)')
             await client.change_presence(status=discord.Status.dnd)
+            fldigi.main.run_macro(7)
+
+        if fldigi.main.get_trx_status() == 'rx':
+            await client.change_presence(status=discord.Status.online, activity=game)
+        else:
+            await client.change_presence(status=discord.Status.dnd, activity=game)
         if fldigi.modem.get_name() != modem:
             game = discord.Game(fldigi.modem.get_name())
             await client.change_presence(status=discord.Status.idle, activity=game)
             modem = fldigi.modem.get_name()
-
+                
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.id == discord_channel_id:
+    if message.channel.id in discord_channel_ids:
         if str(message.author.id) in callsign_bidict:
             callsign = callsign_bidict[str(message.author.id)]
         else:
